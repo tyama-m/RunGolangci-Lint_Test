3be64fec5be566ba4401915c523b9b256e96ac0d
Yamasaki Toshio <tyama@morisawa.co.jp>
2021-10-13 15:42:23 +0900

    docer action test

---
+++ actions/golang-test/entrypoint.sh
@@ -0,0 +1,16 @@
+APP_DIR="/go/src/github.com/${GITHUB_REPOSITORY}/"
+
+mkdir -p "${APP_DIR}" && cp -r ./ "${APP_DIR}" && cd "${APP_DIR}"
+
+export GO111MODULE=on
+go mod tidy
+go mod verify
+
+if [[ "$1" == "lint" ]]; then
+    echo "############################"
+    echo "# Running GolangCI-Lint... #"
+    echo "############################"
+    golangci-lint --version
+    echo
+    golangci-lint run -c .golangci_config.yml --tests=False --timeout=30m --max-issues-per-linter 0 --max-same-issues 0 --out-format=colored-line-number ./...
+fi
